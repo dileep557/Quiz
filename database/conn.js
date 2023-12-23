16807d38a912fbe8eca9de0def9e1fc499251666
yadavdileep <yadavdileep557@gmail.com>
2023-12-23 12:23:14 +0530

    commit

--- database/conn.js
+++ database/conn.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export default async function connect(){
-    await mongoose.connect('mongodb://127.0.0.1:27017');
+    await mongoose.connect(process.env.ATALAS_URL);
     console.log("Database Connected")
 }
